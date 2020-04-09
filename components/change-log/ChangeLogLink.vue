fef202346c074c4434809b06f6d41ea06feb006d
Bartek <bartek.bober.koder@gmail.com>
2020-04-09 02:53:15 +0200

    add change log page and configure router

---
+++ components/change-log/ChangeLogLink.vue
@@ -0,0 +1,3 @@
+<template>
+    <nuxt-link to="/changelog">changelog</nuxt-link>
+</template>
---
+++ pages/changeLog.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid tag="article">
+    <Change-log-ink />
+  </v-container>
+</template>
+
+<script>
+import ChangeLogLink from "@/components/change-log/ChangeLogLink";
+
+export default {
+  components: {
+    'Change-log-ink': ChangeLogLink
+  },
+  data(){
+    return {
+    }
+  },
+  mounted(){
+  },
+  watch: {
+    
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
